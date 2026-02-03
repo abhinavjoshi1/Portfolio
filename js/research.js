1d8ada743b662104492b32ec8e8bbe5a0f6418d9
Abhinav Joshi <abhinavaccess5@gmail.com>
2026-02-03 15:26:55 +0530

    research section, changed method to add papers

---
+++ js/research.js
@@ -0,0 +1,25 @@
+fetch("/research.json")
+  .then(res => res.json())
+  .then(papers => {
+    const container = document.getElementById("research-container");
+    if (!container) return;
+
+    papers.forEach(paper => {
+
+        if (paper.disabled === true) return;
+      const card = document.createElement("a");
+      card.className = "card";
+      card.href = paper.link || "#";
+      card.target = paper.link ? "_blank" : "_self";
+
+      card.innerHTML = `
+        <div>
+          <h3>${paper.title}</h3>
+          <p>${paper.subtitle}</p>
+        </div>
+        <span>${paper.status}</span>
+      `;
+
+      container.appendChild(card);
+    });
+  });
